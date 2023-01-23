780767e79b53f4ad0297d5aae091ecef6c80b08c
wawall99 <77018443+wawall99@users.noreply.github.com>
2023-01-22 19:01:51 -0600

    clean up

--- quickview.js
+++ quickview.js
@@ -12,9 +12,7 @@ document.body.insertAdjacentHTML('afterbegin', quickViewHTML);
 let categoryItems = document.querySelectorAll(".ers-item");
 
 for (const item of categoryItems) {
-    let prefix = 'container_' + folderName +'_id_';
     let itemId = item.id.split("id_")[1];
-    console.log(itemId);
     let quickViewBtn = `<div class="quickview-btn-container"><btn style="width:100%" onclick="quickViewLink(${itemId})" class="quickview-btn ers-item-button button-style">Quick View</btn></div>`;
     let btnContainer = item.querySelector(".button-price-container");
     btnContainer.insertAdjacentHTML("beforebegin", quickViewBtn);
