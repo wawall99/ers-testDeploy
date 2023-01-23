a8ecd542be44018319ca8dad39463508274f7ae9
wawall99 <77018443+wawall99@users.noreply.github.com>
2023-01-22 21:15:31 -0600

    change to object keys

--- quickview.js
+++ quickview.js
@@ -24,7 +24,7 @@ let quickviewSpinner = quickviewOverlay.querySelector(".loader-container");
 
 function quickViewLink(itemId) {
   event.stopPropagation();
-  for (const product of productsData) {
+  for (const product of Object.keys(productsData)) {
     if (product.id == itemId) {
       initQuickView(product.url);
     } 
