ed8ed0db0f37b2d1b927c2940c6f74f322d6999d
wawall99 <77018443+wawall99@users.noreply.github.com>
2023-02-10 18:20:42 -0500

    Change to button-style

--- deliveryArea.js
+++ deliveryArea.js
@@ -30,7 +30,7 @@ function initItemAreaLookup() {
         if (highlightContainer) {
             highlightContainer.insertAdjacentHTML('beforeend', `
                 <div class="clearfix"></div>
-                <p class="check-delivey-btn-wrapper" style="margin-top:10px"><button class="btn button check-delivery-btn">Check Delivery Area</button></p>
+                <p class="check-delivey-btn-wrapper" style="margin-top:10px"><button class="button-style check-delivery-btn">Check Delivery Area</button></p>
             `);
         }
     }
@@ -56,7 +56,7 @@ function initItemAreaLookup() {
                         </div>
                         <div class="map-container"></div>
                         <div class="set-address-btn-container">
-                            <button class="set-address-btn button">Set As Address</button>
+                            <button class="set-address-btn button-style">Set As Address</button>
                             <div class="address-set-wrapper">
                                 <div class="address-set-text">Address Set</div>
                                 <div class="address-set-checkmark">
