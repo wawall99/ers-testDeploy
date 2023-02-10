16ad1f8093d7b04a939b14cb41bda79341a669b4
wawall99 <77018443+wawall99@users.noreply.github.com>
2023-02-10 13:30:10 -0500

    e

--- deliveryArea.js
+++ deliveryArea.js
@@ -241,7 +241,7 @@ function initItemAreaLookup() {
             });
             
             if (showSetAddress) {
-                deliverySetAddressBtn.classList.add('active');
+                deliverySetAddressBtnContainer.classList.add('active');
             }
             
             deliveryResponse.classList.add('active');
@@ -264,7 +264,7 @@ function initItemAreaLookup() {
     function clearDeliveryModel() {
         deliveryOverlay.classList.remove('active');
         deliveryMapContainer.classList.remove('active');
-        deliverySetAddressBtn.classList.remove('active');
+        deliverySetAddressBtnContainer.classList.remove('active');
         deliveryResponse.classList.remove('active');
         deliveryText.innerHTML = ``;
         deliveryMapContainer.innerHTML = ``;
