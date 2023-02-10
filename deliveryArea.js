3af330f527e6f4880d5eeed73dbb79232b7e1e00
wawall99 <77018443+wawall99@users.noreply.github.com>
2023-02-10 17:52:05 -0500

    remove .active

--- deliveryArea.js
+++ deliveryArea.js
@@ -266,6 +266,7 @@ function initItemAreaLookup() {
 
     function saveAddress() {
         localStorage.setItem('addressSave', JSON.stringify(addressSave));
+        deliverySetAddressBtn.classList.add('active');
         const tickMark = "<svg width=\"58\" height=\"45\" viewBox=\"0 0 58 45\" xmlns=\"http://www.w3.org/2000/svg\"><path fill=\"#fff\" fill-rule=\"nonzero\" d=\"M19.11 44.64L.27 25.81l5.66-5.66 13.18 13.18L52.07.38l5.65 5.65\"/></svg>";
         deliverySetAddressBtn.innerHTML = tickMark;   
     }
@@ -274,6 +275,7 @@ function initItemAreaLookup() {
         deliveryOverlay.classList.remove('active');
         deliveryMapContainer.classList.remove('active');
         deliverySetAddressBtnContainer.classList.remove('active');
+        deliverySetAddressBtn.classList.remove('active');
         deliveryAddressSet.classList.remove('active');
         deliveryResponse.classList.remove('active');
         deliveryText.innerHTML = ``;
