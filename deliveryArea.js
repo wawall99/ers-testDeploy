d4af18442bb18a6b00b9ea69214fe02a3afd6ac2
wawall99 <77018443+wawall99@users.noreply.github.com>
2023-02-10 17:40:06 -0500

    add tickmark

--- deliveryArea.js
+++ deliveryArea.js
@@ -266,9 +266,8 @@ function initItemAreaLookup() {
 
     function saveAddress() {
         localStorage.setItem('addressSave', JSON.stringify(addressSave));
-        deliverySetAddressBtn.classList.remove('active');
-        deliveryAddressSet.classList.add('active');
-        
+        const tickMark = "<svg width=\"58\" height=\"45\" viewBox=\"0 0 58 45\" xmlns=\"http://www.w3.org/2000/svg\"><path fill=\"#fff\" fill-rule=\"nonzero\" d=\"M19.11 44.64L.27 25.81l5.66-5.66 13.18 13.18L52.07.38l5.65 5.65\"/></svg>";
+        deliverySetAddressBtn.innerHTML = tickMark;   
     }
 
     function clearDeliveryModel() {
